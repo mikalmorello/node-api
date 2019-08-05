84f76b3dc5662f223a017f18fa55c2b368cf7298
Mikal Morello <mikalmorello@gmail.com>
2019-08-04 22:52:04 -0400

    JS - Log out related course number for instructors in express

--- app.js
+++ app.js
@@ -2,66 +2,91 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
-const path = require('path');
 
 // GET DATA
 
-//const data = require('Courses.json');
 var fs = require('fs');
-var obj;
-fs.readFile('data/Courses.json', 'utf8', function (err, data) {
+var data;
+fs.readFile('data/Courses.json', 'utf8', function (err, contents) {
   if (err) throw err;
-  obj = JSON.parse(data);
+  data = JSON.parse(contents);
 });
 
-//var fs = require('fs');
-//var content = fs.readFileSync('data/Courses.json', 'utf8');
-//try {
-//  const data = JSON.parse(content)
-//  console.log(data);
-//} catch(err) {
-//  console.error(err)
-//}
-//console.log(content);
-
-
-//const fs = require('fs')
-//
-//fs.readFile('data/test.json', 'utf8', (err, fileContents) => {
-//  if (err) {
-//    console.error(err)
-//    return
-//  }
-//  try {
-//    const data = JSON.parse(fileContents)
-//  } catch(err) {
-//    console.error(err)
-//  }
-//})
 
 // USE FUNCTIONS
 
-//var searchCourses = function(req, res, searchTerm){
-//  console.log('searching...' + req);
+//app.use(bodyParser.json());
+
+
+//const logger = (req, res, next) => {
+//  console.log('logging... ' + req.originalUrl );
+//  //console.log(req.params[0]);
+//  next();
 //}
 //
-//app.use(searchCourses);
-
-//app.use(bodyParser.json());
+//app.use(logger);
 
 // ROUTES
 
+// HOME API
 app.get('/', (req, res) => res.send('Hello World!'))
 
+
+// INSTRUCTOR API
 app.get('/courses/instructor/:id', function (req, res) {
-  console.log('instructors are ' + req.params.id ); 
-  res.send('instructors: '+ req.params.id);
+  
+ let searchValue = req.params.id;
+  
+  // Set courses to response
+  let courses = data,
+      coursesMatch = [];
+  // Loop through courses
+  for (const key of Object.keys(courses)) {
+    let course = courses[key],
+        courseId = key,
+        courseArea = courses[key].course_area,
+        courseTerms = courses[key].terms;
+    
+    // Loop through course terms
+    for (const term of Object.keys(courses[key].terms)) {
+      
+      let termInstructors = courses[key].terms[term].instructors;
+      
+      // Search for instructor
+      var instructorMatch = termInstructors.find(function(instructor) {
+        //if(instructor === searchValue){
+        if (searchValue.toLowerCase() === instructor.slice(0, searchValue.length).toLowerCase()) {
+          if (!coursesMatch.includes(courseId)) {
+            coursesMatch.push(courseId);
+          }
+        }
+      });
+    }
+  }
+  if(coursesMatch.length === 0){
+    console.log('instructor does not exist: ' + searchValue);
+  } else {
+    console.log(coursesMatch);
+    console.log(searchValue); 
+  }
+
 })
 
+// COURSES API
+app.get('/courses/:id', function (req, res) {
+  res.send('courses: '+ req.params.id);
+})
+
+
+
+
+
+
+
+
+
 app.get('/test', function (req, res) {
-//  res.json(content);
-//  res.json(data);
-   res.json(obj);
+  res.json(data);
 })
 
 // LISTEN
