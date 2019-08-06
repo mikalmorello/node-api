686573bf1ca05f27b1608f52d523381285cfa13f
Mikal Morello <mmorel01@dhcp-130-64-12-165.medford.tufts.edu>
2019-08-05 20:52:03 -0400

    JS - Cleanup instructor API

--- app.js
+++ app.js
@@ -12,25 +12,11 @@ fs.readFile('data/Courses.json', 'utf8', function (err, contents) {
   data = JSON.parse(contents);
 });
 
-
-// USE FUNCTIONS
-
-//app.use(bodyParser.json());
-
-
-//const logger = (req, res, next) => {
-//  console.log('logging... ' + req.originalUrl );
-//  //console.log(req.params[0]);
-//  next();
-//}
-//
-//app.use(logger);
-
 // ROUTES
 
 // HOME API
 app.get('/', function (req, res) {
-  res.json(data);
+  res.send('homepage');
 });
 
 
@@ -48,74 +34,21 @@ app.get('/courses/instructor/:id', function (req, res) {
     let course = courses[key],
         courseId = key,
         courseArea = courses[key].course_area,
-        courseTerms = courses[key].terms;
+        courseTerms = courses[key].terms,
+        courseNew = {};
+    
+    // create course object
+    courseNew[courseId] = course; 
+    console.log(courseNew);
     
     // Loop through course terms
     for (const term of Object.keys(courses[key].terms)) {
-      
       let termInstructors = courses[key].terms[term].instructors;
       
       // Search for instructor
       var instructorMatch = termInstructors.find(function(instructor) {
         //if(instructor === searchValue){
         if (searchValue.toLowerCase() === instructor.slice(0, searchValue.length).toLowerCase()) {
-          if (!coursesMatch.includes(course)) {
-            coursesMatch.push(course);
-            
-            console.log(courseId);
-            console.log(course);
-//            obj.courseId = course;
-            
-          }
-        }
-      });
-    }
-  }
-  if(coursesMatch.length === 0){
-    console.log('instructor does not exist: ' + searchValue);
-  } else {
-    //console.log(coursesMatch);
-    //console.log(searchValue); 
-    res.json(coursesMatch);
-  }
-
-})
-
-// COURSES API
-app.get('/courses/:id', function (req, res) {
-//  res.send('courses: '+ req.params.id);
-  
- let searchValue = req.params.id;
-  
-  // Set courses to response
-  let courses = Object.entries(data),
-      coursesMatch = [];
-  
-  // Loop through courses
-//  const keys = Object.keys(courses);
-  
-  for (const course of courses) {
-
-    let //course = courses[key],
-        courseId = course[0],
-        courseArea = course[1].course_area,
-        courseTerms = course[1].terms,
-        courseNew = {};
-    
-        // create course object
-        courseNew[course[0]] = course[1]; 
-        console.log(courseNew);
-    
-    // Loop through course terms
-    for (const term of  Object.keys(course[1].terms)) {
-      
-      let termInstructors = course[1].terms[term].instructors;
-      
-      // Search for instructor
-      var instructorMatch = termInstructors.find(function(instructor) {
-        //if(instructor === searchValue){
-        if (searchValue.toLowerCase() === instructor.slice(0, searchValue.length).toLowerCase()) {
-          
           if (!coursesMatch.includes(courseNew)) {
             coursesMatch.push(courseNew);
           }
@@ -124,25 +57,20 @@ app.get('/courses/:id', function (req, res) {
     }
   }
   
+  // Return JSON results
   if(coursesMatch.length === 0){
     console.log('instructor does not exist: ' + searchValue);
   } else {
-//    console.log(coursesMatch);
     res.json(coursesMatch);
   }
+
 })
 
+// COURSES API
+app.get('/courses/:id', function (req, res) {
+  res.send('courses: '+ req.params.id);
+});
 
-
-
-
-
-
-
-
-app.get('/test', function (req, res) {
-  res.json(data);
-})
 
 // LISTEN
 
