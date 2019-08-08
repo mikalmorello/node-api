78c4fd35a80f078e2aad61dc5533fe0f2d259cdd
Mikal Morello <mmorel01@dhcp-130-64-12-165.medford.tufts.edu>
2019-08-07 20:24:50 -0400

    JS - Add cors library

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+
 //const port = 3000;
+app.use(cors());
 
 // Added for Heroku
 const http = require('http');
